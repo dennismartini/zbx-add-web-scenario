7b4974d26f9e3a715b1de07f52eed394a1e926f0
Dennis Di Martini <dennismr.r@gmail.com>
2019-02-08 14:11:30 -0200

    Create add-web-scenario.sh

---
+++ add-web-scenario.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+# Script para cadastrar cenários WEB.
+
+#URL DO ZABBIX (Ex: contoso.com.br/zabbix)
+ZBXURL=$1
+ZBXHOSTNAME=$2
+
+#Usuário e Senha do Zabbix
+USER="$3"
+PASS="$4"
+
+#Nome do Cenário WEB
+CWNAME="$5"
+
+#HostID
+HOSTID="$6"
+
+HEADER='Content-Type:application/json'
+#URL do Zabbix
+URL="http://$ZBXURL/api_jsonrpc.php"
+
+#Nome do Host que o cenário WEB será incluso
+VHOST="$ZBXHOSTNAME"
+
+#Nome do passo do Cenário Web
+NPCW="$7"
+
+#URL Do Cenário Web
+UDCW="$8"
+
+autenticacao()
+{
+    JSON='
+    {
+        "jsonrpc": "2.0",
+        "method": "user.login",
+        "params": {
+            "user": '$USER',
+            "password": '$PASS'
+        },
+        "id": 0
+    }
+    '
+    curl -s -X POST -H "$HEADER" -d "$JSON" "$URL" | cut -d '"' -f8
+}
+
+
+TOKEN=$(autenticacao)
+JSON="
+{
+    "jsonrpc": "2.0",
+    "method": "httptest.create",
+    "params": {
+        "name": "$CWNAME",
+        "hostid": "$6",
+        "steps": [
+                        {
+                "name": "$7",
+                "url": "$8",
+                "status_codes": "$9",
+                "no": 1
+            }
+        ]
+    },
+    "auth": "'$TOKEN'",
+    "id": 1
+}
+"
+
+curl -s -X POST -H "$HEADER" -d "$JSON" "$URL" |jq
